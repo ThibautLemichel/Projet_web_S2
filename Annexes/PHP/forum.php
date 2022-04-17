b8495f5943dc470fdc05fcf456e0e30303f13765
Enzo SERGIANI <enzo.sergiani@student.junia.com>
2022-04-18 00:35:20 +0200

    Travail sur le forum

--- Annexes/PHP/forum.php
+++ Annexes/PHP/forum.php
@@ -1,58 +1,81 @@
 <!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <title>TP4 : web dynamique</title>
-        <meta charset="utf-8" />
-    </head>
-<body>
-    <?php
-        include('connexion.php');
-        $requete = 'SELECT * FROM utilisateurs';//La requere SQL
-			$resultat = mysqli_query($connexion, $requete); //Executer la requete
-			
-			if ( $resultat == FALSE ){
-				echo "<p>Erreur d'exécution de la requete :".mysqli_error($connexion)."</p>" ;
-				die();
-			}
-			else {
-				$nbreLignes = mysqli_num_rows($resultat); //Nombre de ligne du retour de la requete
-				
-				echo "Le Nombre des adhérents est : $nbreLignes <br>";
-				
-				//lire le tableau des resulats ligne par ligne
-				
-				if($nbreLignes>0){
-                    echo "<table>";
-                        echo "<thead>";
-                            echo "<tr>";
-                                echo "<th>id</th>";
-                                echo "<th>Nom</th>";
-                                echo "<th>Prenom</th>";
-                                echo "<th>Pseudo</th>";
-                                echo "<th>Email</th>";
-                                echo "<th>Mot de Passe</th>";
-                                echo "<th>Admin</th>";
-                            echo "</tr>";
-                        echo "</thead>";
-                        echo "<tbody>";
+<link rel="stylesheet" href="../CSS/ProjetChien.css" />
+<link rel="stylesheet" href="../CSS/forum.css"/>
+<html lang="fr-fr">
+<head>
+    <meta charset="utf-8" />
+    <title>Projet chien</title>
+    <meta name="description" content="Trouve le chien de tes rêves" />
+    <meta name="keywords" content="chien, toutou, animaux de compagnies, meilleur ami de l'homme, meilleur ami de la femme" />
+    <meta name="author" content="Thibaut Lemichel" />
+</head>
+    <body>
+    <div id="header">
+        <img id="logo" src="../Images/LogoChien1.jpg" alt="Notre super logo" />
+        <p><a href="../../Index.php">Les toutous</a></p>
+        <nav> <!--Ici commence le menu en haut de la page-->
+            <ul>
+                <li class="menu-deroulant">
+                    <a href="../HTML/Trouvez un chien.html">Trouvez votre chien</a>  <!--grandes pages-->
+                    <ul class="sous-menu">
+                        <li><a href="../HTML/Races.html">Races</a></li>    <!--sous-pages des grandes pages-->
+                        <li><a href="../HTML/Refuges.html">Refuges</a></li>  <!--sous-pages des grandes pages-->
+                    </ul>
+                </li>
+                <li class="menu-deroulant">
+                    <a href="../HTML/Tout savoir sur l'adoption.html">Tout savoir sur l'adoption</a> <!--grandes pages-->
+                    <ul class="sous-menu">
+                        <li><a href="../HTML/Comment bien se préparer.html">Comment bien se preparer à l'arrivée</a></li>  <!--sous-pages des grandes pages-->
+                        <li><a href="../HTML/Pourquoi prendre un chiot.html">Pourquoi prendre un chiot</a></li>  <!--sous-pages des grandes pages-->
+                        <li><a href="../HTML/Les premiers jours de son arrivée.html">Les premiers jours de son arrivée</a></li>  <!--sous-pages des grandes pages-->
+                    </ul>
+                </li>
+                <li class="menu-deroulant">
+                <a href="">Notre communauté</a>
+                    <ul class="sous-menu">
+                        <li><a href="../HTML/Contact.html">Contactez-nous</a></li>  <!--grandes pages-->
+                        <li><a href="../PHP/forum.php">Nos forums</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>  <!--Ici finit le menu en haut de la page-->
+    </div>
+    <h1>Nos forums</h1>
 
-                        while($UneLigne = $resultat->fetch_assoc()){
-                            echo "<tr>";
-                            foreach ($UneLigne as $key=>$value) {
-                                    echo "<td>$UneLigne[$key]</td>";    
-                            }
-                        }        
-                        echo "<tr>";
-                        
-                        echo "</tbody>";
-                    echo "</table>";
-                }
-                else
-                    echo "aucune ligne";
-                
-                mysqli_close($connexion);//Fermer la connexion
-            }
-		?>
-</body>
-
+    <div class="footer">
+        <div class="liste1">
+            <ul>
+                <li>
+                    <a href="../HTML/Trouvez un chien.html">Trouvez votre chien</a>
+                    <ul>
+                        <li><a href="Annexes/HTML/Races.html">Races</a></li>
+                        <li><a href="Annexes/HTML/Refuges.html">Refuges</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="liste2">
+            <ul>
+                <li>
+                    <a href="../HTML/Tout savoir sur l'adoption.html">Tout savoir sur l'adoption</a>
+                    <ul>
+                        <li><a href="../HTML/Comment bien se préparer.html">Comment bien se preparer à l'arrivée</a></li>
+                        <li><a href="../HTML/Pourquoi prendre un chiot.html">Pourquoi prendre un chiot</a></li>
+                        <li><a href="../HTML/Les premiers jours de son arrivée.html">Les premiers jours de son arrivée</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="liste3">
+            <ul>
+                <li><a href="Annexes/HTML/Contact.html">Contactez-nous</a></li>
+            </ul>
+        </div>
+        <div class="social">
+            <a href="https://www.instagram.com/?hl=fr"><img src="../Images/instagram.png" alt="icone instagram" /></a>
+            <a href="https://twitter.com/?lang=fr"><img src="../Images/twitter.png" alt="icone twitter" /></a>
+            <a href="https://fr-fr.facebook.com/"><img src="../Images/facebook.png" alt="icone facebook" /></a>
+        </div>
+    </div>
+    </body>
 </HTML>
